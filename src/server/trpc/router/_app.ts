042d52860f8253eab14c0c4a0ce81326b48eeebf
Emirsyah <muhammademir48@gmail.com>
2023-03-02 08:18:46 +0700

    product table finished

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -3,12 +3,14 @@ import { authRouter } from "./auth";
 import { customerRouter } from "./customer";
 import { exampleRouter } from "./example";
 import { outletRouter } from "./outlet";
+import { productOuter } from './product';
 
 export const appRouter = router({
   // example: exampleRouter,
   outlet: outletRouter,
   customer: customerRouter,
   auth: authRouter,
+  product: productOuter,
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/product.ts
@@ -0,0 +1,77 @@
+import { z } from "zod";
+import { router, protectedProcedure } from "../trpc";
+
+export const productOuter = router({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.products.findMany({
+      include: {
+        outlets: true,
+      },
+    });
+  }),
+  getById: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      })
+    )
+    .query(({ ctx, input }) => {
+      return ctx.prisma.outlets.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        address: z.string(),
+        contact: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.outlets.create({
+        data: {
+          name: input.name,
+          address: input.address,
+          contact: input.contact,
+          total_sales: 0,
+        },
+      });
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+        address: z.string(),
+        contact: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.outlets.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          address: input.address,
+          contact: input.contact,
+        },
+      });
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.outlets.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
