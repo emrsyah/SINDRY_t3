bd8a4b6dff75e887f4f0824d9abe94a10563c016
Emirsyah <muhammademir48@gmail.com>
2023-03-05 20:56:29 +0700

    handle if session is expire

--- src/components/LayoutAdmin.tsx
+++ src/components/LayoutAdmin.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import SidebarAdmin from "./admin/SidebarAdmin";
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const LayoutAdmin = ({ children }: Props) => {
+  const {data, status} = useSession()
+  const route = useRouter()
+
+  useEffect(()=>{
+    if(status === "unauthenticated"){
+      route.replace("/")
+    }
+  }, [])
+
   return (
     <div className="grid grid-cols-[252px_1fr] min-h-screen">
       <nav>
